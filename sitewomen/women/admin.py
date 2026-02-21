1cd03e4031c6d1c4ee30c19c55ce064e7a3ee3e4
oleg <o1egov@mail.ru>
2026-02-21 08:50:24 +0700

    настройка отображения списка статей в админке

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
-from .models import Women
+from .models import Women, Category
 
-admin.site.register(Women)
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    """ класс для настройки отображения статей в админ панели Модель-Women"""
+    list_display = ('id', 'title', 'time_create', 'is_published',"cat")  # список отображаемых полей
+    list_display_links = ('id', 'title')# атрибут делает активными поля в админке
+    ordering = ['time_create','title'] # атрибут список полей по которым сортируем
+    list_editable = ('is_published',)# атрибут определяющий список(кортеж) полей которые можно
+    # редактировать, нужно прописать костыль для статуса " (choices=tuple(map(lambda x: (bool(x[0]), x[1]), "
+    # для преобразования в булевы значения
+    list_per_page = 3 #атрибут устанавливающий количество статей на одной странице
+
+# admin.site.register(Women, WomenAdmin) # добавили в декоратор
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """ класс для настройки отображения Модели -Категории в админ панели"""
+    list_display = ('id', 'name')  # список отображаемых полей
+    list_display_links = ('id', 'name')# атрибут делает активными поля в админке
--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.db.models.fields import CharField
 from django.urls import reverse
 
+
 class PublishedManager(models.Manager):
     """класс создающий пользовательский менеджер, который будет
     возвращать только опубликованные посты"""
+
     def get_queryset(self):
         """метод возвращает все запросы с помощью get_queryset из
          базового класса применяя фильтр, только опубликованные"""
@@ -12,38 +14,37 @@ class PublishedManager(models.Manager):
 
 
 class Women(models.Model):
-
     class Status(models.IntegerChoices):
         """спец класс для определения статуса опубликован или нет"""
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликованный'
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug')
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True,verbose_name='Время изменения')
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                                     default=Status.DRAFT, verbose_name='Статус')
     # создаем атрибут для связывания моделей Many to one(пост и категории)
-    cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name='posts')
+    cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name='posts',verbose_name='Категория')
     # создаем атрибут для связи Many to Many
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name='Теги')
     # создаем атрибут для связи One to One (будет связывать женщин с их мужьями
     # параметр on_delete=models.SET_NULL отвечает если удалить мужа то значение примет Null,
     # blank=True - позволяет поле оставлять пустым, related_name='wuman'- атрибут для обратного связывания
     husband = models.OneToOneField('Husband', on_delete=models.SET_NULL,
-                                   null=True, blank=True, related_name='wuman')
-
+                                   null=True, blank=True, related_name='wuman', verbose_name='Муж')
 
     objects = models.Manager()
-    published = PublishedManager() # создание нового менеджера
+    published = PublishedManager()  # создание нового менеджера
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = "Известные женщины" # Редактируем меняем название в админ панели
-        verbose_name_plural = "Известные женщины"# тоже но чтобы и во множественном числе
+        verbose_name = "Известные женщины"  # Редактируем меняем название в админ панели
+        verbose_name_plural = "Известные женщины"  # тоже но чтобы и во множественном числе
         ordering = ['-time_create']
         indexes = [
             models.Index(fields=['-time_create'])
@@ -52,22 +53,28 @@ class Women(models.Model):
     def get_absolute_url(self):
         """функция помогает выводить полностью посты,
          прописываем в index.html обязательно импортируем """
-        return reverse('post', kwargs={'post_slug':self.slug})
+        return reverse('post', kwargs={'post_slug': self.slug})
+
 
 class Category(models.Model):
     """Определяем модель для категорий"""
 
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
     def __str__(self):
         """в случае обращения к Категории будет выводить ИМя(name)"""
         return self.name
 
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
     def get_absolute_url(self):
         """специальный метод формирует полноценный урл адрес,
         который будет подставляться в list_categories.html"""
-        return reverse('category',kwargs={'cat_slug':self.slug})
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
 
 class TagPost(models.Model):
     """класс для создания Модели для тэгов"""
@@ -82,7 +89,8 @@ class TagPost(models.Model):
     def get_absolute_url(self):
         """специальный метод формирует полноценный урл адрес,
         который будет возвращать тот или иной адрес для конкретного тэга"""
-        return reverse('tag',kwargs={'tag_slug':self.slug})
+        return reverse('tag', kwargs={'tag_slug': self.slug})
+
 
 class Husband(models.Model):
     """создаем модель МУЖ на ней будем изучать связь One to One"""
