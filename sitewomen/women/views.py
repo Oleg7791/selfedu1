925f1ee0b9cf8119d8021a3c6502c40e2df25072
oleg <o1egov@mail.ru>
2026-04-06 18:47:18 +0700

    Шаблонные контекстные процессоры

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -16,7 +16,7 @@ menu = [
     {'title':'О сайте','url_name':'about'},
     {'title':"Добавить статью",'url_name':'add_page'},
     {'title':"Обратная связь",'url_name':'contact'},
-    {'title':"Войти",'url_name':'login'}
+    #{'title':"Войти",'url_name':'login'} прописали в шаблоне base.html
 ]
 
 # заменили на класс WomenHome
