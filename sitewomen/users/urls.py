ea9dea8d2bf3508d18c0f15d638f794e7c326786
oleg <o1egov@mail.ru>
2026-05-03 18:59:25 +0700

    Классы PasswordChangeView и PasswordChangeDoneView -Смена пароля

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 from . import views
 
@@ -12,5 +12,7 @@ urlpatterns = [
     #path('register/', views.register, name='register')
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
+    path('password-change/',views.UserPasswordChange.as_view(), name="password_change"),
+    path('password-change/done/', PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"),name="password_change"),
 ]
 
